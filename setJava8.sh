ccb7e1430c1f7fbffb7c5baf289bcd2d4f3e47dd
dilbrent <dilbrent@gmail.com>
2018-06-17 18:27:48 +0000

    Can't build with Java10 so we have to rig the environment for Java8

---
+++ setJava8.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-armhf
+export JRE_HOME=/usr/lib/jvm/java-8-openjdk-armhf/jre
+export PATH=/usr/lib/jvm/java-8-openjdk-armhf/bin:/usr/lib/jvm/java-8-openjdk-armhf/jre/bin:$PATH
+javac -version
+
